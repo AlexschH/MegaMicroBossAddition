32ae499986802643eb1c5e667ad4fd1bc0b99899
Alexhwa <ah.alswad@gmail.com>
2021-12-17 00:43:20 -0600

    miniboss implementation
    loads miniboss and adds health on win

---
+++ Assets/128-Team Me/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public float spawnInterval;
+    public GameObject spawnObject;
+
+    private int lastIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObject());
+    }
+
+    private IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(spawnInterval);
+
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        while (spawnIndex == lastIndex)
+        {
+            spawnIndex = Random.Range(0, spawnPoints.Length);
+        }
+        Instantiate(spawnObject, spawnPoints[spawnIndex]);
+
+        StartCoroutine(SpawnObject());
+    }
+}
